296f0c678fa6ce1417afff79cc3f3b9e5cd9e291
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-22 19:47:28 +0100

    Create the keypad component

---
+++ src/components/Keypad.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Keypad = () => (
+  <div className="calculator__keypad">
+    <button className="calculator__keypad-btn" type="button">AC</button>
+    <button className="calculator__keypad-btn" type="button">+/-</button>
+    <button className="calculator__keypad-btn" type="button">%</button>
+    <button className="calculator__keypad-btn operation" type="button">÷</button>
+    <button className="calculator__keypad-btn" type="button">7</button>
+    <button className="calculator__keypad-btn" type="button">8</button>
+    <button className="calculator__keypad-btn" type="button">9</button>
+    <button className="calculator__keypad-btn operation" type="button">x</button>
+    <button className="calculator__keypad-btn" type="button">4</button>
+    <button className="calculator__keypad-btn" type="button">5</button>
+    <button className="calculator__keypad-btn" type="button">6</button>
+    <button className="calculator__keypad-btn operation" type="button">-</button>
+    <button className="calculator__keypad-btn" type="button">1</button>
+    <button className="calculator__keypad-btn" type="button">2</button>
+    <button className="calculator__keypad-btn" type="button">3</button>
+    <button className="calculator__keypad-btn operation" type="button">+</button>
+    <button className="calculator__keypad-btn" type="button">0</button>
+    <button className="calculator__keypad-btn" type="button">.</button>
+    <button className="calculator__keypad-btn operation" type="button">=</button>
+  </div>
+);
+
+export default Keypad;

5ccbd4628a40aa5af4286a06b2f495d63c1e68e7
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-25 13:26:44 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Calculator from './components/Calculator';
 
 const App = () => (
-  <div className="container">
-    <Calculator />
-  </div>
+  <>
+    <header>
+      <h1>React Calculator</h1>
+    </header>
+    <main>
+      <Calculator />
+    </main>
+    <footer>
+      <p>fmanimashaun @2023</p>
+    </footer>
+  </>
 );
 
 export default App;
--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Display = () => (
   <form className="calculator__display">
-    <input className="calculator__display-input" type="text" value={0} />
+    <input className="calculator__display-input" type="text" value={0} readOnly />
   </form>
 );
 

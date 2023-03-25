dca868f89b7fb56abb095abb8af8176643d8f881
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-25 18:01:38 +0100

    Update the Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import Keypad from './Keypad';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <section className="calculator">
-    <Display />
-    <Keypad />
-  </section>
-);
+const Calculator = () => {
+  const [calcData, setCalcData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (value) => {
+    const newValue = calcData.next === null ? value : calcData.next + value;
+    setCalcData({ ...calcData, next: newValue });
+  };
+
+  const handleCalculate = (operation) => {
+    const result = calculate(calcData, operation);
+    setCalcData(result);
+  };
+
+  return (
+    <section className="calculator">
+      <Display value={calcData.next || calcData.total || '0'} />
+      <Keypad onButtonClick={handleButtonClick} onCalculate={handleCalculate} />
+    </section>
+  );
+};
 
 export default Calculator;

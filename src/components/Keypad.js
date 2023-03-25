094926f606936c6fc12d50d18872a524d8433483
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-25 18:00:46 +0100

    Add event listener to the buttons

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,27 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Keypad = () => (
+const Keypad = ({ onButtonClick, onCalculate }) => (
   <div className="calculator__keypad">
-    <button className="calculator__keypad-btn" type="button">AC</button>
-    <button className="calculator__keypad-btn" type="button">+/-</button>
-    <button className="calculator__keypad-btn" type="button">%</button>
-    <button className="calculator__keypad-btn operation" type="button">÷</button>
-    <button className="calculator__keypad-btn" type="button">7</button>
-    <button className="calculator__keypad-btn" type="button">8</button>
-    <button className="calculator__keypad-btn" type="button">9</button>
-    <button className="calculator__keypad-btn operation" type="button">x</button>
-    <button className="calculator__keypad-btn" type="button">4</button>
-    <button className="calculator__keypad-btn" type="button">5</button>
-    <button className="calculator__keypad-btn" type="button">6</button>
-    <button className="calculator__keypad-btn operation" type="button">-</button>
-    <button className="calculator__keypad-btn" type="button">1</button>
-    <button className="calculator__keypad-btn" type="button">2</button>
-    <button className="calculator__keypad-btn" type="button">3</button>
-    <button className="calculator__keypad-btn operation" type="button">+</button>
-    <button className="calculator__keypad-btn zero-btn" type="button">0</button>
-    <button className="calculator__keypad-btn" type="button">.</button>
-    <button className="calculator__keypad-btn operation" type="button">=</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onCalculate('AC')}>AC</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onCalculate('+/-')}>+/-</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onCalculate('%')}>%</button>
+    <button className="calculator__keypad-btn operation" type="button" onClick={() => onCalculate('÷')}>÷</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onButtonClick('7')}>7</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onButtonClick('8')}>8</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onButtonClick('9')}>9</button>
+    <button className="calculator__keypad-btn operation" type="button" onClick={() => onCalculate('x')}>x</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onButtonClick('4')}>4</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onButtonClick('5')}>5</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onButtonClick('6')}>6</button>
+    <button className="calculator__keypad-btn operation" type="button" onClick={() => onCalculate('-')}>-</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onButtonClick('1')}>1</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onButtonClick('2')}>2</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onButtonClick('3')}>3</button>
+    <button className="calculator__keypad-btn operation" type="button" onClick={() => onCalculate('+')}>+</button>
+    <button className="calculator__keypad-btn zero-btn" type="button" onClick={() => onButtonClick('0')}>0</button>
+    <button className="calculator__keypad-btn" type="button" onClick={() => onButtonClick('.')}>.</button>
+    <button className="calculator__keypad-btn operation" type="button" onClick={() => onCalculate('=')}>=</button>
   </div>
 );
+
+Keypad.propTypes = {
+  onButtonClick: PropTypes.func.isRequired,
+  onCalculate: PropTypes.func.isRequired,
+};
 
 export default Keypad;

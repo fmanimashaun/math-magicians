daf630ce7e86b7d77b44e3fa202f4e9eec737f7b
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-25 18:01:57 +0100

    Update the App component mark-up

--- src/App.js
+++ src/App.js
@@ -3,15 +3,9 @@ import Calculator from './components/Calculator';
 
 const App = () => (
   <>
-    <header>
-      <h1>React Calculator</h1>
-    </header>
     <main>
       <Calculator />
     </main>
-    <footer>
-      <p>fmanimashaun @2023</p>
-    </footer>
   </>
 );
 

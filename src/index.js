721906765d65fcb31b72ce60ffed5c0289691649
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-30 17:08:08 +0100

    Update file path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/scss/styles.scss';
+import 'assets/scss/styles.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

840ffb71ea876fb378f54fb6ffd67e2639cd9bdc
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-30 23:59:47 +0100

    Update index.js with react routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'assets/scss/styles.scss';
-import App from './App';
+import App from 'components/App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+);

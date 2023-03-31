b350581ae37b7cf7fa4d4182ff8bf506a73a3419
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-31 01:42:09 +0100

    Update the error page component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const NotFound = () => <h1>page not found</h1>;
+const NotFound = () => (
+  <main className="main">
+    <h2 className="main__error-page">Page not found!</h2>
+  </main>
+);
 
 export default NotFound;

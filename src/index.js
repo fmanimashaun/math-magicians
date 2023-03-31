a1d3142677d8f89adbc49f76150c3772d15a5c84
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-31 01:54:24 +0100

    Update basename for the BrowserRouter component

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/math-magicians">
       <App />
     </Router>
   </React.StrictMode>,

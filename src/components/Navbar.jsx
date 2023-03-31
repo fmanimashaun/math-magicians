2eb04e046172078c9070ef387acaf618a50cb156
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-31 01:19:12 +0100

    Update page header component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="calculator">Calculator</Link>
+    </li>
+    <li>
+      <Link to="quote">Quote</Link>
+    </li>
+  </ul>
+);
+
+export default Navbar;
